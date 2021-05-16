8b5b7077ce4f7f0948411912b124066da9927496
exdij <exdij1@gmail.com>
2021-05-16 19:24:01 +0200

    First commit

---
+++ src/main/java/kc/province_identifier/CSVFileParser.java
@@ -0,0 +1,10 @@
+package kc.province_identifier;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class CSVFileParser {
+
+    BufferedReader csvReader = new BufferedReader(new FileReader("src/main/resources/data/Lista(stan_na_01_01_2021).csv"));
+
+}
