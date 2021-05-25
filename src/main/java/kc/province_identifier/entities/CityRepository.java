8be583524a7408e708a17aa92f041715bb1d230b
exdij <exdij1@gmail.com>
2021-05-26 01:10:23 +0200

    explicit encoding defined

--- src/main/java/kc/province_identifier/entities/CityRepository.java
+++ src/main/java/kc/province_identifier/entities/CityRepository.java
@@ -3,9 +3,11 @@ package kc.province_identifier.entities;
 import kc.province_identifier.entities.City;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CityRepository extends CrudRepository<City, Integer> {
     List<City> findByNameContainingIgnoreCase(String name);
+
 }
